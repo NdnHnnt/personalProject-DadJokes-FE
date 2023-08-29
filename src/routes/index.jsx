dce1c4f9ec9c95e1514a4aa2d85143035520edb1
Nadine Haninta <nadinehaninta@gmail.com>
2023-08-29 11:55:01 +0700

    feat:Token + integration

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,10 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { useAuth } from "../provider/authProvider";
 import { ProtectedRoute } from "./ProtectedRoute";
-import Login from "../pages/FormLogin"
-import Logout from "../pages/FormLogout"
+import SignIn from "../pages/FormSignIn";
+import Logout from "../pages/FormLogout";
 import LandingPage from "../pages/LandingPage";
-
+import Dashboard from "../pages/Home";
 
 const Routes = () => {
   const { token } = useAuth();
@@ -17,23 +17,27 @@ const Routes = () => {
     },
     {
       path: "/signin",
-      element: <Login />
+      element: <SignIn />,
     },
   ];
 
   // Define routes accessible only to authenticated users
   const routesForAuthenticatedOnly = [
     {
-      path: "/",
+      path: "/", // This is the protected route wrapper
       element: <ProtectedRoute />, // Wrap the component in ProtectedRoute
       children: [
         {
-          path: "/",
-          element: <div>User Protected</div>,
+          path: "/", // This is the landing page for authenticated users
+          element: <Dashboard />,
         },
         {
           path: "/logout",
           element: <Logout />,
+        },
+        {
+          path: "/signin",
+          redirect: "/",
         },
       ],
     },
@@ -47,16 +51,21 @@ const Routes = () => {
     },
     {
       path: "/signin",
-      element: <Login />,
+      element: <SignIn />,
     },
   ];
 
   // Combine and conditionally include routes based on authentication status
-  const router = createBrowserRouter([
-    ...routesForPublic,
-    ...(!token ? routesForNotAuthenticatedOnly : []),
-    ...routesForAuthenticatedOnly,
-  ]);
+  let authenticatedRoutes = routesForPublic; // Start with public routes
+
+  if (token) {
+    authenticatedRoutes = routesForAuthenticatedOnly;
+  } else {
+    authenticatedRoutes = routesForPublic.concat(routesForNotAuthenticatedOnly);
+  }
+  const router = createBrowserRouter(authenticatedRoutes);
+
+  console.log(token); // This should be placed outside the function call
 
   // Provide the router configuration using RouterProvider
   return <RouterProvider router={router} />;
